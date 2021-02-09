05420c361edc8e92ead77019371dd8c15c4bc4d1
danielszymak <danielszymak@gmail.com>
2021-02-09 12:10:48 +0100

    Actualization of models registration

--- prostozpieca/project/admin.py
+++ prostozpieca/project/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import ProportionBaking, EquipmentBaking, ProcessBaking
+from .models import Bakings, ProportionBaking, EquipmentBaking, ProcessBaking
 # Register your models here.
 
+admin.site.register(Bakings)
 admin.site.register(ProportionBaking)
 admin.site.register(ProcessBaking)
 admin.site.register(EquipmentBaking)

2b929411ff614b1ba2f62c276d3ee392818d6c05
danielszymak <danielszymak@gmail.com>
2021-02-16 10:17:50 +0100

    Model for user's baking attempts created

--- prostozpieca/project/admin.py
+++ prostozpieca/project/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from .models import Bakings, ProportionBaking, EquipmentBaking, ProcessBaking
+from .models import Bakings, ProportionBaking, EquipmentBaking, ProcessBaking, UserBakingAttempt
 # Register your models here.
 
 admin.site.register(Bakings)
 admin.site.register(ProportionBaking)
 admin.site.register(ProcessBaking)
 admin.site.register(EquipmentBaking)
+admin.site.register(UserBakingAttempt)
 
 
